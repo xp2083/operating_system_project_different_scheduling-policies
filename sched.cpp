70ccb3c265bec270851fbd62289b3121356df55b
xp2083 <xp2083@nyu.edu>
2023-02-19 22:59:14 -0500

    first unbug version of fcfs

--- sched.cpp
+++ sched.cpp
@@ -27,6 +27,8 @@ struct Process{
 	int static_prio;
 	int state_ts;
 	int state;
+	int state_prev;
+	int state_prev_prev; 
 };
 
 struct Event{
@@ -35,10 +37,6 @@ struct Event{
 	int old_state;
 	int new_state;
 	int transition;	
-	int cb;
-	int ib;
-	int rem;
-	int prio;
 };
 
 struct MidInfo{
@@ -72,9 +70,9 @@ struct scheduler_fcfs{
 }
 
 Process* fcfs_get_from_queue(struct scheduler* obj, deque<Process*>* run_queue, ifstream* file) {
-        struct scheduler_fcfs *sobj = (struct scheduler_fcfs*)obj; 
-        //printf("%s\n", __FUNCTION__);
-        if (run_queue.size()! = 0)
+	struct scheduler_fcfs *sobj = (struct scheduler_fcfs*)obj; 
+	//printf("%s\n", __FUNCTION__);
+	if (run_queue.size()! = 0)
 		return run_queue.pop_front();
 	else{
 		char tmp [maxVecSize] = {0};
@@ -98,7 +96,7 @@ Process* fcfs_get_from_queue(struct scheduler* obj, deque<Process*>* run_queue, 
 			cnt++;
 		} 	
 		proc->state = STATE_READY;
-	        proc->num = lineNum;
+		proc->num = lineNum;
 		lineNum++;	
 		return proc;			
 	}
@@ -123,12 +121,28 @@ struct function_table fcfs_functions = {
 struct scheduler* sched;  // that's the only object we use in global algo
 struct scheduler_fcfs *fcfs_scheduler; // this is specialized
 
-int myrandom(int burst){
+int my_random(int burst){
 	 return 1 + (randvals[ofs] % burst); 
 }
 
 Event* get_event(deque<Event>* event_queue){
 	return &event_queue->pop_front();
+}
+
+int put_event(deque<Event>* event_queue, Process* process, int old_state){
+        Event event;
+	event.process = process;
+	event.old_state = old_state;
+	if (event.old_state == STATE_RUNNING){
+		event.new_state = STATE_BLOCKED;
+		event.transition = TRANS_TO_BLOCK;
+		event.timestamp = process->state_ts + my_random(process->io_max); 
+	}
+	else if (event.old_state == STATE_BLOCKED){
+		event.new_state	= STATE_READY;
+		event.transition = TRANS_TO_READY;
+		event.timestamp = process->state_ts;
+	}	
 }
 
 int createInfo (MidInfo* info, int start_time, int processNum, int last_time, int prev_state, int next_state,int cb, int ib, int rem, int prio) {
@@ -143,7 +157,6 @@ int createInfo (MidInfo* info, int start_time, int processNum, int last_time, in
 	info->prio = prio;
 
 	return 0;
-
 }
 
 int simulation(ifstream* file){
@@ -165,10 +178,22 @@ int simulation(ifstream* file){
 			case TRANS_TO_READY:
 			//must come from BLOCKED or CREATED 
 			//add to run queue, no event created 
-				CALL_SCHEDULER = true;
-				run_queue -> push_back(proc);
-				MidInfo info;
-				createInfo(&info, proc->stat_ts, proc->num, timeInPrev, )
+			 	CALL_SCHEDULER = true;
+				if (proc->state == STATE_CREATED){
+				//come from CREATED
+					proc->state_prev_prev = STATE_CREATED;
+					proc->state_prev = STATE_READY;
+					proc->state = STATE_RUNNING;
+					run_queue -> push_back(proc);
+				} 
+				else {
+				//come from BLOCKED
+		         	//create the info for RUNNING->BLOCKED					      
+		         	MidInfo info;
+				createInfo(&info, cur_time, proc->num, timeInPrev, proc->state_prev, proc->state_prev_prev,0,timeInPrev, rem = proc->cpu_all_time-timeInPrev, proc->prio);
+				info_vec->push_back(info);
+ 				proc->cpu_all_time = proc->cpu_all_time-timeInPrev;
+				}
 				break;
 			case TRANS_TO_PREEMPT:
 			//must come from RUNNING 
@@ -177,10 +202,20 @@ int simulation(ifstream* file){
 				break;
 			case TRANS_TO_RUN:
 			//create event for either preempt or block
+				put_event(event_queue, proc, STATE_RUNNING); 
+				//create the info for CREATED->READY/BLOCKED->READY 
+				MidInfo info;
+				createInfo(&info, cur_time, proc->num, timeInPrev, proc->state_prev, proc->state_prev_prev, 0, 0, 0, 0);
+				info_vec->push_back(info);
 				break;
 			case TRANS_TO_BLOCK:
-			//create an event fro when process becomes READY again
+			//create an event foo when process becomes READY again
+				put_event(event_queue, proc, STATE_BLOCKED);
 				CALL_SCHEDULER = true;
+				//create the info for READY->RUN
+				MidInfo info;
+				createInfo(&info, cur_time, proc->num, timeInPrev, proc->state_prev, proc->state_prev_prev, timeInPrev, 0, proc->all_cpu_time, proc->prio);
+				info_vec->push_back(info);
 				break;
 			}
 		if(CALL_SCHEDULER){
@@ -193,15 +228,26 @@ int simulation(ifstream* file){
 				if (cur_proc == null)
 					continue;
 				//create event to make this process runnable for same time
-				if (cur_proc -> state == STATE_CREATED){
+				if (cur_proc->state == STATE_CREATED){
 					Event event;
 					event.timestamp = cur_proc->state_ts;
 					event.process = cur_proc;
 					event.old_state = STATE_CREATED;
-					event.new_state = STATE_READY;
+					event.new_state = STATE_CREATED;
 					event.transition = TRANS_TO_READY;
-
 					event_queue -> push_back(event);					 
+				}
+				else {
+				//the state will be STATE_RUNNING
+					Event event;
+					event.timestamp = cur_proc->state_ts;
+					event.process = cur_proc;
+					event.old_state = STATE_READY;
+					event.new_state = STATE_RUNNING;
+					event.transition = TRANS_TO_RUNNING;
+					event_queue -> push_back(event);					 
+	
+
 				}
 			}
 		}
